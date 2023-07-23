0c81fcb289e03df717658c59d9847f24498e26cd
Mahafuj Chowdhury <md.mahafuj012@gmail.com>
2023-07-24 00:49:17 +0600

    /

--- controllers/alumniController.js
+++ controllers/alumniController.js
@@ -7,8 +7,8 @@ export const alumniList = async (req, res, next) =>{
     let searchRgx = {'$regex': req.query.searchKey, $options: 'i'}
     let searchArray = [{firstname: searchRgx},{lastname: searchRgx},{dept: searchRgx},{company: searchRgx},{position: searchRgx}]
     let match = {isAlumni: true}
-    let project = {password:0,email:0,gender:0,isAlumni:0,isAdmin:0,studentId:0,canView:0,createdAt:0,updatedAt:0}
-    let sort = {createdAt: 1}
+    let project = {password:0,email:0,gender:0,isAlumni:0,isAdmin:0,canView:0,createdAt:0,updatedAt:0}
+    let sort = {createdAt: -1}
     let result =await listService(req, User, searchArray, match, project, sort)
     if(result) res.status(200).json(result)
 }
